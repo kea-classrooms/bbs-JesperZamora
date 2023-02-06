1aed52ab0f0dd2f2c698dd16859c7c5fa3dccd61
JesperZamora <113138989+JesperZamora@users.noreply.github.com>
2023-02-06 17:26:23 +0000

    Finished

---
+++ fix-samples.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ cd samples/
+ mv animals/countries/ .
+ mv countries/movies/ .
+ mv animals/Monkies.txt animals/Monkeys.txt 
+ echo Monkeys > animals/Monkeys.txt 
+ mv movies/Germany.txt countries/
+ mv countries/.apes.txt animals/
+ cd -
